df352172928d8f052b93f579367deb46ca76f09d
Keanu Williams <42850481+keanuwilliams@users.noreply.github.com>
2020-09-11 23:27:27 -1000

    Added a navbar and a landing page
    Began creating the eventual layout of the website by creating the base HTML to go off of. Also created a basic navbar to build off of.

--- bfl/log/views.py
+++ bfl/log/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def landing(request):
-    return HttpResponse('<h1>Landing</h1>')
+    return render(request, 'log/pages/landing.html')
