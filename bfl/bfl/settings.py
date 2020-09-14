143d31f4ff7089a1c328deb77fa8875b7e97037c
Keanu Williams <42850481+keanuwilliams@users.noreply.github.com>
2020-09-14 11:47:57 -1000

    Fixed text overlay on picture and added a button and a link

--- bfl/bfl/settings.py
+++ bfl/bfl/settings.py
@@ -118,6 +118,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-
-# Media Files (Images, Video)
-MEDIA_URL = '/media/'
