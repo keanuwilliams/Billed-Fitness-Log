3725d7476183ca362f6e6244915573bed80b0468
Keanu Williams <42850481+keanuwilliams@users.noreply.github.com>
2020-09-14 14:25:22 -1000

    Create template pages for login and signup and made links dynamic

--- bfl/log/urls.py
+++ bfl/log/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('signup/', views.caa, name='create-an-account'),
+    path('login/', views.login, name='login')
 ]
--- bfl/log/views.py
+++ bfl/log/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def landing(request):
     return render(request, 'log/pages/landing.html')
+
+def caa(request):
+    context = {
+        'title': 'Signup'
+    }
+    return render(request, 'log/pages/caa.html', context)
+
+def login(request):
+    context = {
+        'title': 'Login'
+    }
+    return render(request, 'log/pages/login.html', context)
