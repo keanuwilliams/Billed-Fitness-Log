9ca9334b1d3e941d0e77b6181420212c21211c27
Keanu Williams <42850481+keanuwilliams@users.noreply.github.com>
2020-10-01 17:12:33 -1000

    Fixed URLs

--- bfl/log/urls.py
+++ bfl/log/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('', SessionListView.as_view(), name='session-list'),
     path('new/', SessionCreateView.as_view(), name='session-create'),
     path('<int:pk>/', SessionDetailView.as_view(), name='session-detail'),
-    path('post/<int:pk>/edit/', SessionUpdateView.as_view(), name='session-update'),
-    path('post/<int:pk>/delete/', SessionDeleteView.as_view(), name='session-delete'),
+    path('<int:pk>/edit/', SessionUpdateView.as_view(), name='session-update'),
+    path('<int:pk>/delete/', SessionDeleteView.as_view(), name='session-delete'),
 ]
