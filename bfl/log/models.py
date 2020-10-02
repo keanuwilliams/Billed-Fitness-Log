93eebde2ce09ecf80da006d6325ba4a60066a361
Keanu Williams <42850481+keanuwilliams@users.noreply.github.com>
2020-10-01 17:22:51 -1000

    Removed the default text for name of session

--- bfl/log/models.py
+++ bfl/log/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class Session(models.Model):
-    name = models.CharField(max_length=100, default="Workout Session")
+    name = models.CharField(max_length=100)
     date = models.DateTimeField(default=timezone.now)
     workouts = []
     user = models.ForeignKey(User, on_delete=models.CASCADE)
